645e74feb2ccca02cf9147200a24546385293f79
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2023-10-17 19:41:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-	for(let i in sampleObject){
-        if(i === key) return true 
+	for(let i in sampleObject) {
+        if(i === key) {
+            return true;
+        }
     }
     return false ;
 }
